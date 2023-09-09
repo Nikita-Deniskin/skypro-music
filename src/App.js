83c87edf80290eaa680a7a1d789a999d6e31257c
Nikita-Deniskin <ralermr@gmail.com>
2023-09-09 18:24:53 +0500

    4.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import Navigation from './components/NavMenu';
+import Player from './components/AudioPlayer';
+import Sidebar from './components/Sidebar';
+import TrackList from './components/Tracklist';
 
 function App() {
   return (
-    <div className="App">
-      Здесь скоро будет Skypro.Music
+    <div className='App'>
+  <div className="wrapper">
+    <div className="container">
+      <main className="main">
+      < Navigation />
+      < TrackList />
+        < Sidebar />
+      </main>
+      < Player />
+      <footer className="footer" />
     </div>
+  </div>
+  </div>
   );
-}
+};
 
 export default App;
