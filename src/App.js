e97dec5accaddbd5789c2a1a89ae79469797f05c
Nikita-Deniskin <ralermr@gmail.com>
2023-10-30 22:59:52 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+// import * as React from "react";
+import { useState, useEffect } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './components/routes/routes'
+import { AuthContext } from './components/context/context'
 
 function App() {
+  const [isAuth, setIsAuth] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+
+useEffect(() => {
+  setIsAuth(localStorage.getItem("auth")? JSON.parse(localStorage.getItem("auth")) : null)
+  setIsLoading(false)
+}, [])
+
   return (
+    <AuthContext.Provider value={{isAuth, setIsAuth, isLoading}}>
     <BrowserRouter>
       <AppRoutes />
     </BrowserRouter>
+    </AuthContext.Provider>
   )
 }
 
